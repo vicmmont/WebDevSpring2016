4ea957ab900c3a86ebf7d7d4cdc9a3dc5e6a496a
vicmmont <vmm420@gmail.com>
2016-01-22 14:13:14 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require ( 'express' );
-var app = express ();
-var ipaddress = process. env .OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process. env .OPENSHIFT_NODEJS_PORT || 3000 ;
-app .get( '/hello' , function (req, res){
-    res. send ( 'hello world' );
+var express = require('express');
+var app = express();
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000 ;
+app.get( '/hello', function (req, res){
+    res.send('hello world');
 });
-app . listen ( port , ipaddress );
+app.listen(port, ipaddress);
